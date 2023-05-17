1c17312a2ee175630e241129ce17a5509179d352
dsk7758 <dsk2015.dk@gmail.com>
2023-05-17 22:40:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/exp7/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.exp7;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
